a62caf79cfffb3fd34b4e184be3bc28869854f5a
tod619 <trevorodwyer619@hotmail.com>
2023-06-27 22:02:35 +0100

    Create main loop and test all 50 states in main.py file

--- main.py
+++ main.py
@@ -12,18 +12,22 @@ turtle.shape(image)
 # Use pandas to get all the states
 data = pd.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_states = []
 
-# Get the user answer using the textinput
-answer_state = screen.textinput(
-    title="Guess The State", prompt="What's another state name?: ")
 
-# Check if the user answer is in the all_states list
-if answer_state in all_states:
-    t = turtle.Turtle()
-    t.hideturtle()
-    t.penup()
-    state_data = data[data.state == answer_state]
-    t.goto(int(state_data.x), int(state_data.y))
-    t.write(state_data.state)
+while len(guessed_states) < 50:
+    # Get the user answer using the textinput
+    answer_state = screen.textinput(
+        title="Guess The State", prompt="What's another state name?: ")
+
+    # Check if the user answer is in the all_states list
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        guessed_states.append(answer_state)
 
 screen.exitonclick()

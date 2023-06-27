a6100c92f143d4bd86771d905ca84e48da659a61
tod619 <trevorodwyer619@hotmail.com>
2023-06-27 21:44:50 +0100

    Check if user is correct, display name of state in correct location in main.py file

--- main.py
+++ main.py
@@ -9,12 +9,21 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# Use pandas to get all the states
 data = pd.read_csv("50_states.csv")
 all_states = data.state.to_list()
 
-
+# Get the user answer using the textinput
 answer_state = screen.textinput(
     title="Guess The State", prompt="What's another state name?: ")
 
+# Check if the user answer is in the all_states list
+if answer_state in all_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == answer_state]
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(state_data.state)
 
 screen.exitonclick()

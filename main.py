2c7f719b572589b8524639a7bfb678da92aabc0c
tod619 <trevorodwyer619@hotmail.com>
2023-06-27 21:34:18 +0100

    Use pandas to read csv file + save stage column to all_states list in main.py file

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+
 answer_state = screen.textinput(
     title="Guess The State", prompt="What's another state name?: ")
 

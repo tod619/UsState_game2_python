34ecff8383e3c24f9bb27ce68628947fe65e5377
tod619 <trevorodwyer619@hotmail.com>
2023-06-27 21:14:38 +0100

    Add bg image to turtle in main.py file

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import pandas as pd
+import turtle
 
+screen = turtle.Screen()
+screen.title("US States Game")
+
+# Create the background image for the game
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+screen.exitonclick()

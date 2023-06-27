f74040c39628e9cb2158070aaee45a40eac6442a
tod619 <trevorodwyer619@hotmail.com>
2023-06-27 22:23:12 +0100

    generate new csv file with unguessed states if player exits game in main.py file

--- main.py
+++ main.py
@@ -18,8 +18,16 @@ guessed_states = []
 while len(guessed_states) < 50:
     # Get the user answer using the textinput
     answer_state = screen.textinput(
-        title=f"{len(guessed_states)}/50 guessed correctly", prompt="What's another state name?: ")
-
+        title=f"{len(guessed_states)}/50 guessed correctly", prompt="What's another state name?: ").title()
+    # Allow the user to break game loop by typing Exit
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     # Check if the user answer is in the all_states list
     if answer_state in all_states:
         t = turtle.Turtle()
@@ -29,5 +37,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
         guessed_states.append(answer_state)
-
-screen.exitonclick()

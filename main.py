5d3054d31e5ff58110738e7ee412b619e2b9a42f
tod619 <trevorodwyer619@hotmail.com>
2023-06-28 21:02:46 +0100

    Refactor code to use list comprehension to add states to missing_state list in main.py

--- main.py
+++ main.py
@@ -21,10 +21,12 @@ while len(guessed_states) < 50:
         title=f"{len(guessed_states)}/50 guessed correctly", prompt="What's another state name?: ").title()
     # Allow the user to break game loop by typing Exit
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [
+            state for state in all_states if state not in guessed_states]
+        # missing_states = []
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
